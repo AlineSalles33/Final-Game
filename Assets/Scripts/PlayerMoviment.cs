b8c0cef2f9c4b2e71c125912ff6b06bfc5dad9c1
AlineSalles33 <asallesg@student.fitchburgstate.edu>
2025-04-29 10:27:07 -0400

    new sprites

--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -7,6 +7,7 @@ public class PlayerMoviment : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private bool grounded;
+    private bool plataform;
 
     private void Awake()
     {
@@ -19,17 +20,26 @@ public class PlayerMoviment : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && grounded)
             Jump();
+
+        if (Input.GetKey(KeyCode.Space) && plataform)
+            Jump();
     }
 
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, speed);
         grounded = false;
+
+        body.velocity = new Vector2(body.velocity.x, speed);
+        plataform = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
             grounded = true;
+
+        if (collision.gameObject.tag == "FloatingPlataform")
+            plataform = true;
     }
 }
